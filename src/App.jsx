1635d9dfbf2aad0a3a008ecfb12d1904115c2207
Amarildo <amarildokola36@gmail.com>
2025-04-20 01:28:05 +0200

    added router dom/ created the first routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <>
-      <div>Bert Electric</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rreth-nesh" element={<About />} />
+        <Route path="/sherbimet" element={<Services />} />
+        <Route path="/produkte" element={<Products />} />
+        <Route path="/kontakt" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
