bd07d9a2a21e178834db2197a48fba1a0c541a38
Amarildo <amarildokola36@gmail.com>
2025-04-30 01:04:10 +0200

    Converted Navbar and made it from sctach with Tailwind , some other adjustents

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,49 @@
-import { Menubar } from "primereact/menubar";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export default function BasicDemo() {
-  const navigate = useNavigate(); // to navigate programmatically
+export default function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const items = [
-    {
-      label: "Hyrje",
-      icon: "pi pi-home",
-      command: () => navigate("/"),
-    },
-    {
-      label: "Rreth Nesh",
-      icon: "pi pi-users",
-      command: () => navigate("/rreth-nesh"),
-    },
-    {
-      label: "Shërbimet",
-      icon: "pi pi-briefcase",
-      command: () => navigate("/sherbimet"),
-    },
-    {
-      label: "Produktet",
-      icon: "pi pi-shopping-bag",
-      command: () => navigate("/produktet"),
-    },
-    {
-      label: "Na Kontakto",
-      icon: "pi pi-phone",
-      command: () => navigate("/kontakt"),
-    },
+  const navItems = [
+    { label: "Hyrje", path: "/" },
+    { label: "Rreth Nesh", path: "/rreth-nesh" },
+    { label: "Shërbimet", path: "/sherbimet" },
+    { label: "Produktet", path: "/produktet" },
+    { label: "Na Kontakto", path: "/kontakt" },
   ];
 
   return (
-    <div className="card">
-      <Menubar
-        model={items}
-        start={
+    <nav className="bg-gradient-to-r from-[#14128f] via-[#413db6] to-[#14128f] text-[#f4f5fd] px-6 py-2 shadow-md">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <div
+          className="flex items-center cursor-pointer transition-transform duration-200 hover:scale-110"
+          onClick={() => navigate("/")}
+        >
           <img
             src="/BertElectric-Logo.png"
             alt="Bert Electric Logo"
-            className="w-14 h-14 rounded-full ml-6"
+            className="w-18 h-18 rounded-full"
           />
-        }
-        className="justify-around gap-96"
-      />
-    </div>
+        </div>
+
+        {/* Nav Items */}
+        <ul className="flex gap-3 uppercase">
+          {navItems.map((item) => (
+            <li
+              key={item.path}
+              onClick={() => navigate(item.path)}
+              className={`cursor-pointer px-4 py-2 rounded-lg transition-all duration-300 relative ${
+                location.pathname === item.path
+                  ? "bg-[#d1d5db] text-[#14128f] hover:scale-110 hover:shadow-xl"
+                  : "hover:bg-[#d1d5db] hover:text-[#14128f] hover:scale-110 hover:shadow-xl"
+              }`}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   );
 }
