2102f59bb8f91f23e10505786e48d08f09eaa951
Amarildo <amarildokola36@gmail.com>
2025-05-08 00:59:13 +0200

    little adj

--- src/components/homePage/Hero.jsx
+++ src/components/homePage/Hero.jsx
@@ -4,13 +4,13 @@ export default function Hero() {
       {/* Hero Image */}
       <img className="w-full h-90 opacity-80" src="./hero.jpeg" />
 
-      {/* Company Title & Description */}
-      <section className=" bg-[#413db6] text-[#f4f5fd] py-20 px-6 shadow-inner">
+      {/* Title & Description */}
+      <section className=" bg-[#413db6] text-[#f4f5fd] py-20 px-6">
         <div className="max-w-4xl mx-auto text-center">
-          <h1 className="text-5xl md:text-6xl font-extrabold tracking-wide drop-shadow-lg">
+          <h1 className="text-4xl md:text-5xl font-extrabold tracking-wide drop-shadow-lg">
             Bert Electric
           </h1>
-          <p className="mt-6 text-lg md:text-xl font-medium leading-relaxed text-[#e0e1fc]">
+          <p className="mt-6 text-md md:text-lg font-medium leading-relaxed text-[#e0e1fc]">
             Ekspertë në instalime elektrike, kamera monitorimi dhe sisteme
             sigurie. Ofrojmë vetëm cilësi të lartë me produkte ekskluzive nga{" "}
             <span className="font-semibold text-white">Dahua</span>,{" "}
